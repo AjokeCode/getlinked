f83b5e429e3e8030553e61f9f00ec2859c74d055
Zawjahabdmaalik <ruqayyahalabi536@gmail.com>
2023-09-23 23:53:28 +0100

    getlinked4

--- src/header/header.js
+++ src/header/header.js
@@ -43,6 +43,7 @@ const Header = ()=>{
             </ul>
             
                <BsListNested className='open' onClick={()=> setIsClick(true)}/>
+               
                {isClick ?
                 <>
               <div className='ham-div'>
